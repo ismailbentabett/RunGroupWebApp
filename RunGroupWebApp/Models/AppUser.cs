14de89e7b1e4b3376cf79a3a1c83712ca40fd249
Ismail Bentabet <ismailbentabett@gmail.com>
2022-12-24 15:30:09 +0100

    Add project files.

---
+++ RunGroupWebApp/Models/AppUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RunGroupWebApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public  Adress? Adress { get; set; }
+        public int? Pace { get; set; }
+
+        public int? Mileage { get; set; }
+
+
+    }
+}
